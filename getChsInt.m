f032abc518044c39976f2ca8987a24eb3e1ff17f
ScientificProgrammer <39175541+ScientificProgrammerOwner@users.noreply.github.com>
2022-08-14 11:28:56 +0800

    Update getChsInt.m

--- getChsInt.m
+++ getChsInt.m
@@ -1,5 +1,17 @@
 function nstr = getChsInt(str)
 
+%{
+    Author(作者): 巴山(bashan)
+    WeChat Official Account(微信公众号): 懂科学的程序员(ScientificProgrammer)
+    Github: https://github.com/ScientificProgrammerOwner
+    Youtube: https://www.youtube.com/channel/UC4U9cwe6FxRKec_0s5z0Wkw
+    ZhiHu(知乎): https://www.zhihu.com/people/67-93-61-26-8
+    BiliBili(B站): https://space.bilibili.com/205153905
+
+    Welcome to subscribe! 欢迎订阅！
+    
+%}
+
 % 转换金额的整数部分 Convert integer part
 nstr = [];           % 存储转换结果 Store the results of conversion 
 nzeroFlag = true;    % 是否为零的标识 the flag of whether it is zero or not
